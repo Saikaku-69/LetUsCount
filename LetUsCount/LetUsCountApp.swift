60e1083c6998dae11881dc4f5cfd24caa69c0d05
Saikaku-69 <grnmnsta@gmail.com>
2024-11-13 12:14:26 +0900

    first update

---
+++ LetUsCount/LetUsCountApp.swift
@@ -0,0 +1,17 @@
+//
+//  LetUsCountApp.swift
+//  LetUsCount
+//
+//  Created by cmStudent on 2024/11/13.
+//
+
+import SwiftUI
+
+@main
+struct LetUsCountApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
